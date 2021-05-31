c0551ebc9eba6b3fe2eb1e779dbdd91c63671c18
olcayarpac <olcayarpac@yahoo.com>
2021-05-31 11:17:40 +0300

    try catch

---
+++ try_catch/try_catch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "hello";
+            string s2 = "world";
+
+            try
+            {
+                int a = 5;
+                int b = 7;
+
+                double x = a;
+                Console.WriteLine(x);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
